5362e1fcde1455ec3544809d7c1db24bb99ed530
AGR <agr@octo.com>
2019-10-09 10:57:19 +0200

    adds iam role for authenticating on the api through Google OIDC

--- deploy/iam/topology.tf
+++ deploy/iam/topology.tf
@@ -1,6 +1,6 @@
 variable "env" {}
 
-resource "aws_iam_policy" "policy" {
+resource "aws_iam_policy" "s3_presigned_urls_role" {
   name        = "hallebarde-${var.env}-s3-presigned-url"
   path        = "/hallebarde/"
   description = "Allows hallebarde to create s3 presigned urls in hallebarde bucket"
@@ -18,6 +18,30 @@ resource "aws_iam_policy" "policy" {
             "Resource": "${data.aws_s3_bucket.bucket.arn}"
         }
     ]
+}
+EOF
+}
+
+resource "aws_iam_role" "api_authent_role" {
+  name = "hallebarde-${var.env}-api-authent"
+  path = "/hallebarde/"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "accounts.google.com"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "accounts.google.com:aud": "274210205449-bend56f57m2gcu9an3q6dmv0atj3i7h1.apps.googleusercontent.com"
+        }
+      }
+    }
+  ]
 }
 EOF
 }
