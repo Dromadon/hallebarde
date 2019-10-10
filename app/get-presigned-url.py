665256298ca7a73e9ccd91d0657753f976e0f993
AGR <agr@octo.com>
2019-10-10 15:09:10 +0200

    first version of lambda returning valid s3 presigned url for fixed object

---
+++ app/get-presigned-url.py
@@ -0,0 +1,20 @@
+import boto3
+import json
+
+def handler(context, event):
+  s3_client = boto3.client('s3')
+  try:
+      response = s3_client.generate_presigned_post("hallebarde-storage-dev",
+                                                   "test",
+                                                   Fields=None,
+                                                   Conditions=None,
+                                                   ExpiresIn=600)
+  except ClientError as e:
+      logging.error(e)
+      return None
+
+  final_response = { "isBase64Encoded": False, "body": json.dumps(response), "headers": None, "statusCode": 200 }
+
+  # The response contains the presigned URL and required fields
+  return final_response
+
