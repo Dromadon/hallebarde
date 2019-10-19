5f34a01b9a3486581252edad42cae186f914dcd2
Mehdi Houacine <mhouacine@octo.com>
2019-10-19 14:25:48 +0200

    REFACTO: Refactoring the authorizer +token provider and adding some tests
    REFACTO: Refactoring the token provider
    
    QUALITY: Adding mypy and flake8 for quality and code style inspection

--- app/hallebarde/get_presigned_url.py
+++ app/hallebarde/get_presigned_url.py
@@ -1,20 +1,24 @@
-import boto3
 import json
+import logging
+from typing import Optional
 
-def handle(event, context):
-  s3_client = boto3.client('s3')
-  try:
-      response = s3_client.generate_presigned_post("hallebarde-storage-dev",
-                                                   "test",
-                                                   Fields=None,
-                                                   Conditions=None,
-                                                   ExpiresIn=600)
-  except ClientError as e:
-      logging.error(e)
-      return None
+import boto3
+from botocore.exceptions import ClientError
 
-  final_response = { "isBase64Encoded": False, "body": json.dumps(response), "headers": None, "statusCode": 200 }
 
-  # The response contains the presigned URL and required fields
-  return final_response
+def handle(event: dict, context) -> Optional[dict]:
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.generate_presigned_post("hallebarde-storage-dev",
+                                                     "test",
+                                                     Fields=None,
+                                                     Conditions=None,
+                                                     ExpiresIn=600)
+    except ClientError as e:
+        logging.error(e)
+        return None
 
+    final_response = {"isBase64Encoded": False, "body": json.dumps(response), "headers": None, "statusCode": 200}
+
+    # The response contains the presigned URL and required fields
+    return final_response
--- app/tests/test_get_token.py
+++ app/tests/test_get_token.py
@@ -1,18 +1,14 @@
-import unittest
+from unittest import TestCase
 from unittest.mock import patch
 
 from hallebarde.get_token import handle
 
 
-class TestToken(unittest.TestCase):
+class TestToken(TestCase):
 
     def test_handle_should_return_a_correctly_formed_lambda_proxy_response(self):
-        # Given
-        event = {}
-        context = {}
-
         # When
-        response = handle(event, context)
+        response: dict = handle(event={}, context={})
 
         # Then
         assert isinstance(response['isBase64Encoded'], bool)
@@ -21,17 +17,22 @@ class TestToken(unittest.TestCase):
         assert isinstance(response['statusCode'], int)
 
     @patch('hallebarde.get_token.generate_token')
-    def test_handle_should_return_upload_and_download_token_in_body(self, mock_generate_token):
+    def test_handle_should_return_upload_and_download_token_in_its_body(self, mock_generate_token):
         # Given
-        event = {}
-        context = {}
         expected_upload_token = "9WsE02XcMAekmjKHoj9Zq7_uVqc8dVz1Fq8czbZOq_Y"
         expected_download_token = "2yOdhL1NM5lyX7Mt8pHVYfkE51UkoSuGUrv6z4OT-c4"
         mock_generate_token.side_effect = [expected_upload_token, expected_download_token]
 
         # When
-        response = handle(event, context)
+        response: dict = handle(event={}, context={})
 
         # Then
-        assert response['body']['upload_token'] == expected_upload_token
-        assert response['body']['download_token'] == expected_download_token
+        assert response == {
+            "isBase64Encoded": False,
+            "body": {
+                "upload_token": expected_upload_token,
+                "download_token": expected_download_token
+            },
+            "headers": None,
+            "statusCode": 200
+        }
