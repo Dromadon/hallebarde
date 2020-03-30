824ac497e73200fdd6e419e7d795998ddaa506dc
AGR <dromadairefever@gmail.com>
2020-03-30 22:03:13 +0200

    add logging

--- app/hallebarde/revoke_expired_exchanges.py
+++ app/hallebarde/revoke_expired_exchanges.py
@@ -10,11 +10,14 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
-def handle(event: dict, context: dict) -> dict:
+def handle(event: dict, context: dict) -> None:
     maximum_creation_time = datetime.now(timezone.utc) - timedelta(days=config.EXPIRATION_DELAY_IN_DAYS)
+    logger.log(f'Revoking exchanges before {maximum_creation_time}')
     exchanges_to_revoke = exchange_repository.get_before_time(maximum_creation_time)
+    logger.log(f'Revoking {len(exchanges_to_revoke)} exchanges')
 
     for e in exchanges_to_revoke:
+        logger.log(f'Revoking exchange {e.identifier}')
         revoke_exchange_command_handler.handle(e.identifier)
 
 
