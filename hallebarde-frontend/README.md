5b338768523c51967d75673f17f3cb02555cf377
M <mehdi.houacine7@gmail.com>
2020-04-08 18:05:23 +0200

    Creating Login view & sign-in button with VueJS (#35)
    Co-authored-by: Mehdi Houacine <mhouacine@octo.com>

---
+++ hallebarde-frontend/README.md
@@ -0,0 +1,25 @@
+# hallebarde-frontend
+
+## Project setup
+
+```sh
+$> make install-dependencies
+```
+
+### Compiles and hot-reloads for development
+
+```sh
+$> npm run serve
+```
+
+### Compiles and minifies for production
+
+```sh
+$> make package
+```
+
+### Lints and fixes files
+
+```sh
+$> npm run lint
+```
---
+++ hallebarde-frontend/src/components/LoginView.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id="login">
+    <h1 class="title">
+      Bienvenue sur le partage de fichiers !
+    </h1>
+    <h2 class="subtitle is-size-6">
+      Envoi de fichiers sécurisés permettant de mettre à disposition un lien de
+      <br />
+      téléchargement pendant 7 jours à vos clients.
+    </h2>
+    <SignIn />
+  </section>
+</template>
+
+<script>
+import SignIn from "@/components/SignIn";
+
+export default {
+  name: "LoginView",
+  components: { SignIn }
+};
+</script>
